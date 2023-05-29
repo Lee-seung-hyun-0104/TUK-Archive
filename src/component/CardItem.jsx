d146fcf3bd15e3efbc39d6a6054640476dbc6ab2
silverji0215 <85821258+silverji0215@users.noreply.github.com>
2023-05-29 10:25:59 +0900

    Update CardItem.jsx

--- src/component/CardItem.jsx
+++ src/component/CardItem.jsx
@@ -1,28 +1,27 @@
 import React from "react";
-
-import styles from './CardItem.module.css'
-
+import styles from './Card.module.css'
 
 function CardItem(props){
 
   return(
-    <div className={styles.CardItem}>
-      <div className={styles.background}>{props.image}</div>
+    <div className={styles.card} onClick={props.onClick}>
+      <div className={styles.img}><img src={props.img} alt="작품 이미지" /></div>
       <div className={styles.content}>
         <div className={styles.subject}>
-            <p>{props.subject}</p>
+          <p className={styles.subjectTitle}>{props.subject}</p>
         </div>
-        <div className={styles.name}>
-            <p className={styles.username}>{props.userName}</p>
-            <h2 className={styles.title}>{props.title}</h2>
+        <div className={styles.info}>
+          <p className={styles.name}>{props.userName}</p>
+          <h2 className={styles.title}>{props.title}</h2>
         </div>
+
         <div className={styles.like}>
-            <div className={styles.icon}></div>
-            <div className={styles.number}>{props.like}</div>
+          <div className={styles.icon}>❤</div>
+          <p className={styles.number}>{props.number}</p>
         </div>
       </div>
     </div>
   )
 }
 
-export default CardItem
+export default CardItem;
